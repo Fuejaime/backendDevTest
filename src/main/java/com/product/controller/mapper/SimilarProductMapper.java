c71677cd2bc3def31116e594ccffcfe090e255c3
jaimegomez <jaimegfuenmayor@gmail.com>
2025-03-19 22:26:21 +0100

    feat: fixing Mapping, no constant problem

--- src/main/java/com/product/controller/mapper/SimilarProductMapper.java
+++ src/main/java/com/product/controller/mapper/SimilarProductMapper.java
@@ -1,11 +1,16 @@
 package com.product.controller.mapper;
 
+import com.product.domain.model.ProductDetail;
 import com.product.domain.model.SimilarProduct;
+import com.product.infrastructure.entity.ProductDetailEntity;
 import com.product.infrastructure.entity.SimilarProductEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface SimilarProductMapper {
 
     SimilarProduct toDomain(SimilarProductEntity entity);
+
+    ProductDetail toDomain(ProductDetailEntity entity);
 }
--- src/main/java/com/product/infrastructure/mapper/ProductMapper.java
+++ src/main/java/com/product/infrastructure/mapper/ProductMapper.java
@@ -3,6 +3,8 @@ package com.product.infrastructure.mapper;
 import com.product.infrastructure.entity.ProductDetailEntity;
 import com.product.infrastructure.adapter.response.MockClientResponse;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface ProductMapper {
