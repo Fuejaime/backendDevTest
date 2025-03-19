6393288eaa2e46fa877acb461afabc4a58192e22
jaimegomez <jaimegfuenmayor@gmail.com>
2025-03-19 23:10:08 +0100

    feat: fixing Mapping, adding circuit breaker

--- src/main/java/com/product/infrastructure/ProductServiceImpl.java
+++ src/main/java/com/product/infrastructure/ProductServiceImpl.java
@@ -5,11 +5,14 @@ import com.product.infrastructure.adapter.MockClient;
 import com.product.infrastructure.adapter.response.MockClientResponse;
 import com.product.infrastructure.entity.ProductDetailEntity;
 import com.product.infrastructure.mapper.ProductMapper;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ProductServiceImpl implements ProductService {
@@ -17,17 +20,28 @@ public class ProductServiceImpl implements ProductService {
     private final MockClient mockClient;
     private final ProductMapper productMapper;
 
+    private static final String PRODUCT_SERVICE = "productService"; // Nombre del circuito
 
     @Override
+    @CircuitBreaker(name = PRODUCT_SERVICE, fallbackMethod = "fallbackGetDetailById")
     public ProductDetailEntity getDetailById(String productId) {
         final MockClientResponse mockClientResponse = mockClient.findProductDetail(productId);
-
         return productMapper.asProductDetail(mockClientResponse);
-
     }
 
     @Override
+    @CircuitBreaker(name = PRODUCT_SERVICE, fallbackMethod = "fallbackGetSimilarIdListById")
     public List<Integer> getSimilarIdListById(String productId) {
         return mockClient.findSimilarIds(productId);
+    }
+
+    public ProductDetailEntity fallbackGetDetailById(String productId, Throwable throwable) {
+        log.error("Fallo en la obtención del detalle del producto con ID: {}. Error: {}", productId, throwable.getMessage());
+        return new ProductDetailEntity();
+    }
+
+    public List<Integer> fallbackGetSimilarIdListById(String productId, Throwable throwable) {
+        log.error("Fallo al obtener la lista de ID similares para el producto con ID: {}. Error: {}", productId, throwable.getMessage());
+        return List.of();
     }
 }
