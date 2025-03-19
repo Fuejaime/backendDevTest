7f449e9ffc81c83e1187403fcabbfd77f5d894d2
jaimegomez <jaimegfuenmayor@gmail.com>
2025-03-19 14:35:07 +0100

    feat: Add unit test for controllerTest

---
+++ src/test/java/com/product/controller/ProductsControllerTest.java
@@ -0,0 +1,48 @@
+package com.product.controller;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+
+
+import com.product.domain.ProductUseCase;
+import com.product.infrastructure.entity.ProductDetail;
+import com.product.infrastructure.entity.SimilarProducts;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class ProductsControllerTest {
+    @Mock
+    private ProductUseCase productsUseCase;
+    @Mock
+    private com.product.controller.mapper.SimilarProductsMapper similarProductsMapper;
+    @InjectMocks
+    private ProductController productsController;
+    final private static String PRODUCT_ID = "1";
+    @Nested
+    class GetProductsById{
+        @Test
+        void when_getProductsByIdCallIsSuccessful_then_returnOKResponseEntity(){
+            final SimilarProducts similarProducts = SimilarProducts.builder()
+                    .details(List.of(ProductDetail.builder()
+                            .id(PRODUCT_ID)
+                            .name("name")
+                            .price(100)
+                            .availability(true)
+                            .build()))
+                    .build();
+
+            when(productsUseCase.provideSimilarProducts(PRODUCT_ID)).thenReturn(similarProducts);
+            productsController.getProductsById(PRODUCT_ID);
+
+            verify(similarProductsMapper).areSimilarProductsDTO(similarProducts);
+        }
+    }
+
+}
